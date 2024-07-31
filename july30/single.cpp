973f9a770e14916bd76470d9c36e4f16b572457a
devendra <devendrakurve094@gmail.com>
2024-07-31 18:04:57 +0530

    1stcommit

---
+++ july30/single.cpp
@@ -0,0 +1,29 @@
+// #include <iostream>
+// using namespace std ;
+// int main ()
+// {
+//     int a;
+//     cout<<"ente meter reading\n";
+//     cin>>a;
+//     if (a>0)
+//     {
+//         cout<<"your reading="<<a;
+//     }
+    
+// }
+
+#include <iostream>
+using namespace std ;
+int main ()
+{
+    int a;
+    cout<<"ente ur percentage\n";
+    cin>>a;
+    if (a>60 && a<=100)
+    {
+        cout<<"1st divsion ="<<a <<"%";
+        cout<<"congrates";
+    }
+    cout<<"done";
+    
+}
