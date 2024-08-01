41f21f62f96ae3b0119b3f65df345f5e05dd855d
devendra <devendrakurve094@gmail.com>
2024-08-01 19:18:23 +0530

    2nd commit

---
+++ july31bicondition/bicondition.cpp
@@ -0,0 +1,193 @@
+// wap to find max value
+
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int a,b;
+//     cout <<"enter 1st num\n"; 
+//     cin>>a;
+//     cout <<"enter 2nd num\n";
+//     cin>>b;
+//     if (a>b)
+//     {
+//         cout <<"max value"<<a;
+//     }
+//     else{
+//         cout<<"small value"<<b;
+//     }
+// }
+
+// wap to even and odd
+
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int a;
+//     cout <<"enter any num\n";
+//     cin>>a;
+
+//     if (a%2==0)
+//     {
+//         cout <<"even";
+//     }
+//     else{
+//         cout<<"odd";
+//     }
+// }
+
+// wap to vovel and consonent
+
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     char c;
+//     cout <<"enter any char\n";
+//     cin>>c;
+
+//     if (c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||c=='A'||c=='E'||c=='I'||c=='O'||c=='U')
+//     {
+//         cout <<"vowel";
+//     }
+//     else{
+//         cout<<"consonent";
+//     }
+// }
+
+// wap to gender male or female
+
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//   char g;
+//   cout<<"enter gender\n";
+//   cin>>g;
+//   if (g=='m'||g=='M')
+//   {
+//     cout<<"male";
+//   }
+//   if (g=='F'||g=='f')
+//   {
+//     cout<<"female";
+//   }
+// }
+
+// wap to given number is divisal by 2 and 11 otherwise not divisal
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int a;
+//     cout << "enter any num\n";
+//     cin >> a;
+//     if (a % 2 == 0 && a % 11 == 0)
+//     {
+//         cout << "it is divisal by 2 and 11 num";
+//     }
+//     else
+//     {
+//         cout << "not divisal 2 and 11 num";
+//     }
+// }
+
+
+//wap single expression question
+
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     if (2)      
+//     {
+//         cout<<"welcome";
+//     }
+//     else{
+//         cout<<"bye";
+//     }  
+// }
+
+//wap multiple  expression question
+
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     if (7,0)      
+//     {
+//         cout<<"welcome";
+//     }
+//     else{
+//         cout<<"bye";
+//     }
+// }
+
+
+//wap to expresion question
+// #include<iostream>
+// using namespace std ;
+// int main()
+// {
+//     int pwd;
+//     cout<<"enter 4 digit password\n";
+//     cin>>pwd;
+//     if (1000)
+//     {
+//         cout<<"valid";
+//     }
+//     else{
+//         cout<<"invalid";
+//     }
+// }
+
+//wap to expresion question
+// #include<iostream>
+// using namespace std ;
+// int main()
+// {
+    
+//     if ("hello")
+//     {
+//         cout<<"valid";
+//     }
+//     else{
+//         cout<<"invalid";
+//     }
+// }
+
+//wap to expresion question
+// #include<iostream>
+// using namespace std ;
+// int main()
+// {
+    
+//     if (cout<<"hello")
+//     {
+//         cout<<"valid";
+//     }
+//     else{
+//         cout<<"invalid";
+//     }
+// }
+
+
+//wap to check givan password is exactly 4 digit number if it is found display valid else in valid
+
+// #include<iostream>
+// using namespace std ;
+// int main()
+// {
+//     int pwd;
+//     cout<<"enter 4 digit password\n";
+//     cin>>pwd;
+//     if (pwd>=1000&&pwd<=9999)
+//     {
+//         cout<<"valid";
+//     }
+//     else{
+//         cout<<"invalid";
+//     }
+// }
