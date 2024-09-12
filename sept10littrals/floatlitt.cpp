438c08c9a196a25e069bc78d5948447a23ebddce
devendra <devendrakurve094@gmail.com>
2024-09-12 18:42:16 +0530

    csmkj

---
+++ sept10littrals/floatlitt.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    // float f=34.65e-2;   // float literal
+    // cout<<f<<"\n";;
+    // float g=34.56E2;  // float literal
+    // cout<<g<<"\n";
+
+    char d='joys';    // character literal 
+    cout<<d<<"\n"    //  output = s
+    char f[]="joy";  // string literal
+    cout<<f<<"\n";   // 
+    cout<<sizeof(f); //size = 2
+}
---
+++ sept10littrals/litterals.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a=0x28;    //0x28   hexadecimal
+    cout<<"hexadecimal="<<a<<"\n";
+    int b=034;    //octal
+    cout<<"octal="<<b<<"\n";
+    int c=23;      //decimal
+    cout<<"decimal="<<c;
+}
