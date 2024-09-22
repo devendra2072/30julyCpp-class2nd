71d918f9b0ab590c6342483e45720677ca02b7d2
devendra <devendrakurve094@gmail.com>
2024-09-22 16:05:34 +0530

    je

--- sept18_2_dimantion/twod_arry.cpp
+++ sept18_2_dimantion/twod_arry.cpp
@@ -26,7 +26,7 @@
 //     }
 // }
 
-// wap to print 
+// wap to print
 // #include <iostream>
 // using namespace std;
 // int main()
@@ -43,7 +43,7 @@
 //             cin >> arr[r][c];
 //         }
 //     }
-    
+
 //     cout<<"output=\n";
 //     for (int r = 0; r < 3; r++)
 //     {
@@ -79,8 +79,51 @@
 //         for (int c = 0; c < 3; c++)
 //         {
 //             sum=sum+arr[r][c];
-            
+
 //         }
 //     }
 //     cout <<sum;
 // }
+
+// waqp to display the replace 0
+
+// wap to diplay the sum of the values of two arry
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int arr[2][3] = {1, 2, 3, 4, 5, 6};
+//     int arr2[2][3] = {1, 1, 1, 1, 1, 1};
+//     cout << "values=\n";
+//     for (int r = 0; r < 2; r++)
+//     {
+//         for (int c = 0; c < 3; c++)
+//         {
+//             cout << arr[r][c] + arr2[r][c] << "\t";
+//         }
+//         cout << "\n";
+//     }
+// }
+
+
+
+// 3 dimantion array 
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr [2][2][3]={1,2,3,4,5,6,7,8,9,10,11,12};
+    for (int t = 0; t < 2; t++)
+    {
+        for (int r = 0; r < 2; r++)
+        {
+         for (int c = 0; c < 3; c++)
+         {
+        cout<<arr[t][r][c]<<"\t";
+         }
+         cout<<"\n";
+        }
+        cout<<"\n";
+    }   
+}
