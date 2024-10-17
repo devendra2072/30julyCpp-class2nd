09e4b6c1cd2dd16471fe59e98eda1b8cc6724dfa
devendra <devendrakurve094@gmail.com>
2024-10-17 19:01:36 +0530

    17 octumber fun overloadinf

--- oct_14_opps_concept/opps.cpp
+++ oct_14_opps_concept/opps.cpp
@@ -114,36 +114,140 @@
 //     obj.table(x);
 // }
 
-// ismain class name nahi diya hai 
-#include <iostream>
-using namespace std;
-class 
-{
-public:
-    void sum(int a, int b) // member function
-    {
-        cout << "sum=" << a + b << "\n";
-    }
-    void table(int d)
-    {
-        for (int i = 1; i <= 10; i++)
-        {
-            cout << i * d << "\t";
-        }
-    }
-} obj;
-int main()
-{
-    int x, y;
-    cout << "enter 2 number=";
-    cin >> x >> y;
-    obj.sum(x, y);
-    cout << "enter any number=";
-    cin >> x;
-    obj.table(x);
-}
-// types of classes
+// ismain class name nahi diya hai
+// #include <iostream>
+// using namespace std;
+// class
+// {
+// public:
+//     void sum(int a, int b) // member function
+//     {
+//         cout << "sum=" << a + b << "\n";
+//     }
+//     void table(int d)
+//     {
+//         for (int i = 1; i <= 10; i++)
+//         {
+//             cout << i * d << "\t";
+//         }
+//     }
+// } obj;
+// int main()
+// {
+//     int x, y;
+//     cout << "enter 2 number=";
+//     cin >> x >> y;
+//     obj.sum(x, y);
+//     cout << "enter any number=";
+//     cin >> x;
+//     obj.table(x);
+// }
+
+
+//========================================= types of classes===================
 // 1 single class
 
 // 2 anonymous class
 // a class witch dosen not have  its own name as anonymous class
+
+// #include <iostream>
+// using namespace std;
+// class h1
+// {
+//     int c;
+
+// public:
+//     int sum(int x, int y)
+//     {
+//         c = x + y;
+//         return c;
+//     }
+//     void show()
+//     {
+//         cout << "sum=" << c << "\n";
+//     }
+// };
+
+// int main()
+// {
+//     h1 obj;
+//     cout << obj.sum(2, 5);
+//     obj.show();
+// }
+
+// function decleare inside the  class and define outside the class
+// scope resoluation :: oprator
+
+// #include <iostream>
+// using namespace std;
+// int a = 90;
+// int main()
+// {
+//     int a = 9;
+//     cout << ::a;
+// }
+
+
+
+// #include <iostream>
+// using namespace std;
+
+// class cybrom {
+//     int c;
+//     public:int sum(int x,int y); // we have initialized the function;
+//     void show();
+
+// };
+// int cybrom::sum(int x,int y){ // we have declared the functions
+// // we cannot re-Define a function once its already define
+// // We can only define the function only if the function is declared;
+//      c = x+y;
+//      return c;
+
+// }
+// void cybrom::show(){
+//     cout<<c;
+// }
+
+// int main() {
+//     cybrom obj;
+//    int x,y;
+//    cout<<"Enter two numbers";
+
+//      cin>>x>>y;
+//      obj.sum(x,y);
+//      obj.show();
+
+//     return 0;
+// }
+
+
+
+
+
+
+
+
+#include <iostream>
+using namespace std;
+class h1
+{
+int c;
+public:int sum(int x, int y)
+    {
+        c=x+y;
+    }
+    void show()
+    {
+        cout<<c;
+    }
+};
+
+int main()
+{
+    h1 obj;
+    obj.sum(2, 5);
+    cout<<obj.c;
+}
+
+
---
+++ oct_17_overloading/fun_over.cpp
@@ -0,0 +1,33 @@
+// function overloading
+// signature kahte hai 
+// if a class having more then one function with the same name but using diffrent is called function overloading in a class
+// #include<iostream>
+// using namespace std;
+// class student
+// {
+//     public:void sum()
+//     {
+//         cout<<"sum program\n";
+
+//     }
+//     void sum (int a, int b)
+//     {
+//         cout<<"result: "<<a+b<<"\n";
+
+//     }
+// };
+// int main()
+// {
+//     student s;
+//     s.sum();
+//     s.sum(5,6);
+// }
+
+// static function
+
+#include<iostream>
+using namespace std;
+class student
+{
+    public:static 
+}
